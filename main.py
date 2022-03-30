2c33ba3edd40cd431f60f20ab6682df66d6a808f
iyanumanuel <e.iyanuloluwa@gmail.com>
2022-03-30 18:09:24 +0100

    edited gitignore

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+from yt_stats import YTstats
 
 API_KEY = "AIzaSyC9yZ1phQzITxSCUPqpM1GQkiowLa7gHOA"
+channel_id = 'UCV2xi_w10k6ewdPVxDP6uCQ'
+
+yt = YTstats(API_KEY, channel_id)
+
+yt.get_channel_statistics()
