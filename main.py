cf588e60cf7068e273010ff451c5dbaefb6f469c
iyanumanuel <e.iyanuloluwa@gmail.com>
2022-03-31 22:15:01 +0100

    modifed script to get channel videos

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ API_KEY = "AIzaSyC9yZ1phQzITxSCUPqpM1GQkiowLa7gHOA"
 channel_id = "UCV2xi_w10k6ewdPVxDP6uCQ"
 
 yt = YT_stats(API_KEY, channel_id)
-data = yt.get_channel_statistics()
-yt.dump()
+#data = yt.get_channel_statistics()
+#yt.dump()
 # print(data)
+
+yt.get_channel_video_data()
--- youtube_stats.py
+++ youtube_stats.py
@@ -20,11 +20,52 @@ class YT_stats:
         #print(data)
         try:
             data = data["items"][0]["statistics"]
-        except:
+        except KeyError:
             data = None
 
         self.channel_statistics = data
         return data
+    
+    def get_channel_video_data(self):
+        # 1) get video ids
+        channel_videos = self._get_channel_videos(limit=50)
+        print(len(channel_videos))
+
+        # 2) get video statistics, TODO: next video
+
+    def _get_channel_videos(self, limit=None):
+        url = f'https://www.googleapis.com/youtube/v3/search?key={self.api_key}&channelId={self.channel_id}&part=id&order=date'
+        if limit is not None and isinstance(limit , int):
+            url += '&maxResults=' + str(limit)
+#        print(url)
+        video, nextPageToken = self._get_channel_videos_per_page(url)
+        idx = 0
+        while (nextPageToken is not None and idx < 10):
+            nexturl = url + "&pageToken=" + nextPageToken
+            next_video, nextPageToken = self._get_channel_videos_per_page(nexturl)
+            video.update(next_video)
+            idx+= 1
+
+        return video
+
+    def _get_channel_videos_per_page(self, url):
+        json_url = requests.get(url)
+        data = json.loads(json_url.text)
+        channel_videos = dict()
+        if 'items' not in data:
+            return channel_videos, None
+        
+        item_data = data['items']
+        next_page_token = data.get('nextPageToken', None)
+        for item in item_data:
+            try:
+                kind = item['id']['kind']
+                if kind == 'youtube#video':
+                    video_id = item['id']['videoId']
+                    channel_videos[video_id] = dict()
+            except KeyError:
+                print('error')
+        return channel_videos, next_page_token
 
     def dump(self):
         if self.channel_statistics is None:
