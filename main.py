9f9be57e1d15204f0fd7f0da0c3a4e19d755df7c
Iyanumanuel <e.iyanuloluwa@gmail.com>
2022-03-31 23:40:49 +0100

    created jupyter notebook to analyze data

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ API_KEY = "AIzaSyC9yZ1phQzITxSCUPqpM1GQkiowLa7gHOA"
 channel_id = "UCV2xi_w10k6ewdPVxDP6uCQ"
 
 yt = YT_stats(API_KEY, channel_id)
-#data = yt.get_channel_statistics()
-#yt.dump()
+yt.get_channel_statistics()
+yt.get_channel_video_data()
+yt.dump()
 # print(data)
 
-yt.get_channel_video_data()
+#yt.get_channel_video_data()
--- youtube_stats.py
+++ youtube_stats.py
@@ -1,6 +1,6 @@
 import requests 
 import json
-
+from tqdm import tqdm
 # from ssl import CHANNEL_BINDING_TYPES
 # from main import API_KEY
 
@@ -11,6 +11,7 @@ class YT_stats:
         self.api_key = api_key
         self.channel_id = channel_id
         self.channel_statistics = None
+        self.video_data = None
 
     def get_channel_statistics(self):
         url = f'https://www.googleapis.com/youtube/v3/channels?part=statistics&id={self.channel_id}&key={self.api_key}'
@@ -31,7 +32,30 @@ class YT_stats:
         channel_videos = self._get_channel_videos(limit=50)
         print(len(channel_videos))
 
-        # 2) get video statistics, TODO: next video
+        # 2) get video statistics
+        parts = ['snippet', 'statistics', 'contentDetails']
+        for video_id in tqdm(channel_videos):
+            for part in parts:
+                data = self._get_single_video_data(video_id, part)
+                channel_videos[video_id].update(data)
+        
+        self.video_data = channel_videos
+        return channel_videos
+
+
+    def _get_single_video_data(self, video_id, part):
+        url = f'https://www.googleapis.com/youtube/v3/videos?part={part}&id={video_id}&key={self.api_key}'
+        json_url = requests.get(url)
+        data = json.loads(json_url.text)
+        try:
+            data = data['items'][0][part]
+        except:
+            print('error')
+            data = dict()
+
+        return data
+
+
 
     def _get_channel_videos(self, limit=None):
         url = f'https://www.googleapis.com/youtube/v3/search?key={self.api_key}&channelId={self.channel_id}&part=id&order=date'
@@ -68,14 +92,17 @@ class YT_stats:
         return channel_videos, next_page_token
 
     def dump(self):
-        if self.channel_statistics is None:
+        if self.channel_statistics is None or self.video_data is None:
+            print('data is none')
             return 
+
+        fused_data = {self.channel_id: {'channel_statistics': self.channel_statistics, 'video_data': self.video_data}}
         
-        channel_title = "HofchurchNG"  # TODO: get channel name from
+        channel_title = self.video_data.popitem()[1].get('channelTitle', self.channel_id)
 #        channel_title = channel_title.replace(" ", "_").lower()
         file_name = channel_title + '.json'
         with open(file_name, 'w') as f:
-            json.dump(self.channel_statistics, f, indent=4)
+            json.dump(fused_data, f, indent=4)
 
         print('file dumped')
 
